54adf734da1d51dfee5d16aaaa60ab9ea610fec9
kodieh <khill152@mail.snu.edu>
2016-10-10 14:04:41 -0500

    working java, minus accidental cloning

--- src/edu/snu/csne/p/GeneticAlgorithm.java
+++ src/edu/snu/csne/p/GeneticAlgorithm.java
@@ -23,14 +23,12 @@ public class GeneticAlgorithm
 		int bool = 0, totalFitness = 0, maxFitness = 80, size = 10;
 		ArrayList<Citizen> geneArray = new ArrayList<Citizen>();
 		ArrayList<Citizen> nextPop = new ArrayList<Citizen>(size);
-		ArrayList<Citizen> holder;
 		Citizen father, mother, son;
 		int loop = 0;
 		Util build = new Util();
 
 		geneArray = build.Builder();
 		
-		holder = geneArray;
 		while(totalFitness != maxFitness)
 		{
 		    
@@ -38,7 +36,9 @@ public class GeneticAlgorithm
 			{			
 				bool = rand.nextInt((0-0+1) + 0);
 				father = build.rouletteWheel(geneArray);
-				mother = build.rouletteWheel(holder);
+				geneArray.remove( father );
+				mother = build.rouletteWheel(geneArray);
+				geneArray.add( father );
 				son = build.crossover(father, mother, bool);
 				geneArray.set(i, son);
 			}
